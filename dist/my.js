1e09f4058f491ebc3ad81a7363b8ac7379eaa770
yanmi <yanmi@gmail.com>
2021-03-21 19:15:22 +0800

    提交

---
+++ dist/my.js
@@ -0,0 +1,15 @@
+function addFavorite() {
+    var title = '51modao磨刀网实时地址发布页';
+    var url = 'https://51modao.github.io/';
+    try {
+        window.external.addFavorite(url, title);
+    }
+    catch (e) {
+        try {
+            window.sidebar.addPanel(title, url, "");
+        }
+        catch (e) {
+            alert("抱歉，您所使用的浏览器无法完成此操作。\n\n加入收藏失败，请使用Ctrl+D进行添加");
+        }
+    }
+}
